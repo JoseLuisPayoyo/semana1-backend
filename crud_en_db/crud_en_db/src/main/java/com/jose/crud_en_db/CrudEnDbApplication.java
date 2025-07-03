f12e0e1617d120f249cea8288a44fe3fb2be84fc
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-07-03 16:11:27 +0200

    Proyecto iniciado

---
+++ crud_en_db/crud_en_db/src/main/java/com/jose/crud_en_db/CrudEnDbApplication.java
@@ -0,0 +1,13 @@
+package com.jose.crud_en_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEnDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEnDbApplication.class, args);
+	}
+
+}
---
+++ crud_en_db/crud_en_db/src/test/java/com/jose/crud_en_db/CrudEnDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.crud_en_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEnDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
