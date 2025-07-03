1e54dbd1d50d9b03490c395afee63e1dd1266b2b
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-07-03 10:43:59 +0200

    Proyecto creado

---
+++ primer_crud/src/test/java/com/primer_crud/primer_crud/PrimerCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primer_crud.primer_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
