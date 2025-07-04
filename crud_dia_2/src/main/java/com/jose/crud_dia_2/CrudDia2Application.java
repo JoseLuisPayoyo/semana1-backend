6d6e88f096ce6a82bad7099b50539773f3abe981
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-07-04 16:35:44 +0200

    Initialize Spring Boot project with Maven setup and basic application structure

---
+++ crud_dia_2/src/main/java/com/jose/crud_dia_2/CrudDia2Application.java
@@ -0,0 +1,13 @@
+package com.jose.crud_dia_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDia2Application.class, args);
+	}
+
+}
---
+++ crud_dia_2/src/test/java/com/jose/crud_dia_2/CrudDia2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.crud_dia_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDia2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
