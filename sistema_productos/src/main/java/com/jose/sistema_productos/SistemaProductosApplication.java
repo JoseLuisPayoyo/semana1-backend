bb7be9a09665740fdd4d689e4c22cc98c3c8ac2a
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-07-05 11:43:29 +0200

    proyecto iniciado

---
+++ sistema_productos/src/main/java/com/jose/sistema_productos/SistemaProductosApplication.java
@@ -0,0 +1,13 @@
+package com.jose.sistema_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaProductosApplication.class, args);
+	}
+
+}
