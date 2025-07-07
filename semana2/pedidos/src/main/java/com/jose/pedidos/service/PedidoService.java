8943a45e8a9b2ef70f1d483eb74e06e365b81b48
Jose Luis Rodriguez <joseluis.rodriguez@escuelaestech.es>
2025-07-07 13:03:08 +0200

    Implement methods in PedidoService for managing Pedido entities

--- semana2/pedidos/src/main/java/com/jose/pedidos/service/PedidoService.java
+++ semana2/pedidos/src/main/java/com/jose/pedidos/service/PedidoService.java
@@ -2,11 +2,16 @@ package com.jose.pedidos.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.jose.pedidos.repository.PedidoRepository;
+import com.jose.pedidos.util.PedidoMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jose.pedidos.dto.PedidoDTO;
+import com.jose.pedidos.exception.RecursoNoEncontradoException;
 import com.jose.pedidos.model.Cliente;
 import com.jose.pedidos.model.Pedido;
 import com.jose.pedidos.repository.ClienteRepository;
@@ -15,18 +20,44 @@ import com.jose.pedidos.repository.ClienteRepository;
 public class PedidoService implements IPedidoService{
 
     @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @Autowired
     private ClienteRepository clienteRepository;
 
-    @Override
-    public PedidoDTO guardarPedido(PedidoDTO pedidoDTO) {}
+    // PedidoService(PedidoRepository pedidoRepository) {
+    //     this.pedidoRepository = pedidoRepository;
+    // }
 
     @Override
-    public List<PedidoDTO> listarPedido() {}
+    public PedidoDTO guardarPedido(PedidoDTO pedidoDTO) {
+        Cliente cliente = clienteRepository.findById(pedidoDTO.getClienteId())
+            .orElseThrow(() -> new RecursoNoEncontradoException("Cliente no encontrado con id: " + pedidoDTO.getClienteId()));
+        Pedido pedido = PedidoMapper.toEntity(pedidoDTO, cliente);
+        Pedido pedidoGuardado = pedidoRepository.save(pedido);
+
+        return PedidoMapper.toDTO(pedidoGuardado);
+    }
 
     @Override
-    public PedidoDTO buscarPedidoPorId(Long id) {}
+    public List<PedidoDTO> listarPedido() {
+        return pedidoRepository.findAll()
+            .stream()
+            .map(PedidoMapper::toDTO)
+            .collect(Collectors.toList());
+    }
 
     @Override
-    public void eliminarPedido(Long id) {}
+    public PedidoDTO buscarPedidoPorId(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarPedidoPorId'");
+    }
+
+    @Override
+    public void eliminarPedido(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarPedido'");
+    }
+
     
 }
