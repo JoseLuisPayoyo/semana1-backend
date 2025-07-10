e3ac77338308a24454993103cc93307b8de9d411
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-10 10:57:33 +0200

    Proyecto Iniciado

---
+++ semana2/gestionusuarios/src/test/java/com/payoyo/gestionusuarios/GestionusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payoyo.gestionusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
