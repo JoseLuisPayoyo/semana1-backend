130eeabe291f479e20922813415188d0764d01d3
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-10 13:05:41 +0200

    fix: servicio de prodcto

--- semana2/gestionusuarios/src/main/java/com/payoyo/gestionusuarios/service/ProductoService.java
+++ semana2/gestionusuarios/src/main/java/com/payoyo/gestionusuarios/service/ProductoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.payoyo.gestionusuarios.dto.ProductoRequestDTO;
 import com.payoyo.gestionusuarios.dto.ProductoResponseDTO;
@@ -12,6 +13,7 @@ import com.payoyo.gestionusuarios.exception.RecursoNoEncontradoException;
 import com.payoyo.gestionusuarios.mapper.ProductoMapper;
 import com.payoyo.gestionusuarios.repository.ProductoRepository;
 
+@Service
 public class ProductoService implements IProductoService{
 
     @Autowired
