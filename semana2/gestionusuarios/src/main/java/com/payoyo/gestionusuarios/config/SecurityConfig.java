d374e4eed83cfedc5712b1bae1c2ecf62f4e7657
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-10 12:25:09 +0200

    fix: probado con postman el registro y login

--- semana2/gestionusuarios/src/main/java/com/payoyo/gestionusuarios/config/SecurityConfig.java
+++ semana2/gestionusuarios/src/main/java/com/payoyo/gestionusuarios/config/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -42,4 +47,18 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .authenticationProvider(authenticationProvider());
+
+        return http.build();
+    }
+
 }
