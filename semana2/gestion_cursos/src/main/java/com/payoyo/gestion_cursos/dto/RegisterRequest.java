3e51b8438173aafd1056ed71988be819ad8e7ee0
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-10 18:35:43 +0200

    feat: dtos

---
+++ semana2/gestion_cursos/src/main/java/com/payoyo/gestion_cursos/dto/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.payoyo.gestion_cursos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String nombre;
+    private String email;
+    private String password;
+}
