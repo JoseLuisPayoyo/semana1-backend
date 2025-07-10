c30b30a3f5a3de633c57c072c05b9bec6d62fccd
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-10 11:22:04 +0200

    feat: entidad de usuario

---
+++ semana2/gestionusuarios/src/main/java/com/payoyo/gestionusuarios/entity/Usuario.java
@@ -0,0 +1,38 @@
+package com.payoyo.gestionusuarios.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role rol;
+
+}
