f6b31a079a3c9f866f993b88c45280957f8a4111
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-10 12:08:40 +0200

    feat: dtos

---
+++ semana2/gestionusuarios/src/main/java/com/payoyo/gestionusuarios/dto/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.payoyo.gestionusuarios.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
---
+++ semana2/gestionusuarios/src/main/java/com/payoyo/gestionusuarios/dto/RegisterRequest.java
@@ -0,0 +1,16 @@
+package com.payoyo.gestionusuarios.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String nombre;
+    private String email;
+    private String password;
+}
