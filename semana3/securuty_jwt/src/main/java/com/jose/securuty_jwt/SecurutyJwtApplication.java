a15f633ece31a56195d3df45b6075ee26d6190de
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-14 16:13:52 +0200

    Semana 3

---
+++ semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/SecurutyJwtApplication.java
@@ -0,0 +1,13 @@
+package com.jose.securuty_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurutyJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurutyJwtApplication.class, args);
+	}
+
+}
---
+++ semana3/securuty_jwt/src/test/java/com/jose/securuty_jwt/SecurutyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.securuty_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurutyJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
