bb8b0c255a6e7471f27fee14cdb342fcaecc0c2f
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-14 18:13:10 +0200

    sin seguridad

--- semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/controllers/PrincipalController.java
+++ semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/controllers/PrincipalController.java
@@ -5,10 +5,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,8 @@ import com.jose.securuty_jwt.request.CreateUserDTO;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class PrincipalController {
 
     @Autowired
@@ -36,7 +40,7 @@ public class PrincipalController {
         return "Hola seguro";
     }
 
-    @PostMapping
+    @PostMapping("/createUser")
     public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserDTO createUserDTO){
         
         Set<RoleEntity> roles = createUserDTO.getRoles().stream()
--- semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/repositories/RoleRepository.java
+++ semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/repositories/RoleRepository.java
@@ -3,5 +3,7 @@ package com.jose.securuty_jwt.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.jose.securuty_jwt.models.RoleEntity;
+
 @Repository
-public interface RoleRepository extends CrudRepository<RoleRepository, Long>{}
+public interface RoleRepository extends CrudRepository<RoleEntity, Long>{}
