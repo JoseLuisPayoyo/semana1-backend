98e48df3f63c01e58b07574e7eb373ad2377ff6e
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-14 17:51:48 +0200

    feat: repo de roles

---
+++ semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.jose.securuty_jwt.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<RoleRepository, Long>{}
