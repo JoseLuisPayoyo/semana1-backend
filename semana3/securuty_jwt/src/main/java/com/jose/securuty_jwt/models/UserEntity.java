9134c63f4362258e33c03cc7502600ce4ebb9d70
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-14 17:45:42 +0200

    termiandas entidades

--- semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/models/UserEntity.java
+++ semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/models/UserEntity.java
@@ -1,9 +1,16 @@
 package com.jose.securuty_jwt.models;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -37,5 +44,10 @@ public class UserEntity {
     @NotBlank
     private String password;
 
-    
+    @ManyToMany(fetch = FetchType.EAGER, targetEntity = RoleEntity.class, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "user_roles", 
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleEntity> roles;
 }
