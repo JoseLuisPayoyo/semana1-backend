fb4dfbf36a1055975ae54e42485d906789919d3d
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-15 10:45:26 +0200

    cambios

--- semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/models/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.jose.securuty_jwt.models;
-
-public enum ERole {
-    ADMIN, 
-    USER,
-    INVITED
-}
--- semana3/securuty_jwt/src/main/java/com/jose/securuty_jwt/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jose.securuty_jwt.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.jose.securuty_jwt.models.RoleEntity;
-
-@Repository
-public interface RoleRepository extends CrudRepository<RoleEntity, Long>{}
