6b700a1d47b01f1e0c4013f59903b2121116ced3
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-15 11:16:10 +0200

    feat: relación ManyToOne entre Pedido y Cliente

--- semana3/clientes-pedidos/src/main/java/com/payoyo/clientes_pedidos/model/Pedido.java
+++ semana3/clientes-pedidos/src/main/java/com/payoyo/clientes_pedidos/model/Pedido.java
@@ -7,6 +7,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,5 +30,9 @@ public class Pedido {
     private String descripcion;
     private LocalDate fecha;
     private BigDecimal total;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
     
 }
