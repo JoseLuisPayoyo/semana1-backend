8a1c67264ba3081ee0622c77acb3dcd30bbe8361
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-15 12:05:41 +0200

    fix: eliminado cliente vacío en PedidoMapper para evitar inserciones erróneas

---
+++ semana3/clientes-pedidos/src/main/java/com/payoyo/clientes_pedidos/mapper/PedidoMapper.java
@@ -0,0 +1,31 @@
+package com.payoyo.clientes_pedidos.mapper;
+
+import com.payoyo.clientes_pedidos.dto.PedidoDTO;
+import com.payoyo.clientes_pedidos.model.Cliente;
+import com.payoyo.clientes_pedidos.model.Pedido;
+
+public class PedidoMapper {
+
+    public PedidoDTO toDTO(Pedido pedido){
+        PedidoDTO pedidoDTO = new PedidoDTO();
+
+        pedidoDTO.setId(pedido.getId());
+        pedidoDTO.setDescripcion(pedido.getDescripcion());
+        pedidoDTO.setFecha(pedido.getFecha());
+        pedidoDTO.setTotal(pedido.getTotal());
+        
+        return pedidoDTO;
+    }
+
+    public Pedido toEntity(PedidoDTO pedidoDTO){
+        Pedido pedido = new Pedido();
+        
+        pedido.setId(pedidoDTO.getId());
+        pedido.setDescripcion(pedidoDTO.getDescripcion());
+        pedido.setFecha(pedidoDTO.getFecha());
+        pedido.setTotal(pedidoDTO.getTotal());
+
+        return pedido;
+    }
+    
+}
