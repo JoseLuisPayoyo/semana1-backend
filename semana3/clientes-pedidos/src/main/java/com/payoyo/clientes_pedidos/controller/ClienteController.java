3da143c30296f8bafc7b150148b4037b615a3c3c
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-15 12:20:01 +0200

    feat:cliente controlador

---
+++ semana3/clientes-pedidos/src/main/java/com/payoyo/clientes_pedidos/controller/ClienteController.java
@@ -0,0 +1,36 @@
+package com.payoyo.clientes_pedidos.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.payoyo.clientes_pedidos.dto.ClienteDTO;
+import com.payoyo.clientes_pedidos.service.IClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    
+    @Autowired
+    private IClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> crear(@RequestBody ClienteDTO clienteDTO){
+        ClienteDTO creado = clienteService.crearCliente(clienteDTO);
+        return ResponseEntity.ok(creado);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteDTO> obtener(@PathVariable Long id) {
+        Optional<ClienteDTO> cliente = clienteService.obtenerCliente(id);
+        return cliente.map(ResponseEntity::ok)
+                      .orElse(ResponseEntity.notFound().build());
+    }
+}
