c6e6402693c1e1821420ac877906ed5e115fadd5
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-16 11:47:17 +0200

    proyecto iniciado

---
+++ 01-crud-week1/gestion_ferreteria/src/main/java/com/payoyo/gestion_ferreteria/GestionFerreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.payoyo.gestion_ferreteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFerreteriaApplication.class, args);
+	}
+
+}
---
+++ 01-crud-week1/gestion_ferreteria/src/test/java/com/payoyo/gestion_ferreteria/GestionFerreteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payoyo.gestion_ferreteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFerreteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
