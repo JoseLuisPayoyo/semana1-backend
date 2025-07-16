64e81490883c1f1b14b5f81d18cf1ed60199272e
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-16 12:46:56 +0200

    feat: mapper

---
+++ 01-crud-week1/gestion_ferreteria/src/main/java/com/payoyo/gestion_ferreteria/mapper/ProductoMapper.java
@@ -0,0 +1,31 @@
+package com.payoyo.gestion_ferreteria.mapper;
+
+import com.payoyo.gestion_ferreteria.dto.CreateProductoDTO;
+import com.payoyo.gestion_ferreteria.dto.ProductoResponseDTO;
+import com.payoyo.gestion_ferreteria.model.Producto;
+
+public class ProductoMapper {
+    
+    public static Producto toEntity(CreateProductoDTO dto){
+        Producto producto = new Producto();
+        
+        producto.setNombre(dto.getNombre());
+        producto.setCategoria(dto.getCategoria());
+        producto.setPrecio(dto.getPrecio());
+        producto.setStock(dto.getStock());
+        producto.setActivo(true);
+
+        return producto;
+    }
+
+    public static ProductoResponseDTO toDTO(Producto producto) {
+        return new ProductoResponseDTO(
+            producto.getId(),
+            producto.getNombre(),
+            producto.getCategoria(),
+            producto.getPrecio(),
+            producto.getStock(),
+            producto.getActivo()
+        );
+    }
+}
