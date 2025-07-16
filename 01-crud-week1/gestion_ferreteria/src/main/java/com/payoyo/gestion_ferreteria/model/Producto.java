bdae043b8cec3d899b202619a42655e0d4520d83
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-16 11:52:26 +0200

    feat: entidad producto

---
+++ 01-crud-week1/gestion_ferreteria/src/main/java/com/payoyo/gestion_ferreteria/model/Producto.java
@@ -0,0 +1,32 @@
+package com.payoyo.gestion_ferreteria.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "productos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private Double precio;
+    private Integer stock;
+    private String categoria;
+    private Boolean activo;
+    
+    
+}
